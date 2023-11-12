5cbbde9739239dd52c6deb5aaa56f113881335bd
Kubiat <jszymanski373@gmail.com>
2023-11-12 18:12:01 +0100

    changed font size on the win modal

--- src/projects/memory/index.js
+++ src/projects/memory/index.js
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 function Memory() {
     const navigate = useNavigate()
-    const CARDS_AMOUNT = 6
+    const CARDS_AMOUNT = 1
     const ANIMAL_LIST = ['frog', 'fox', 'cat', 'dog', 'capibara', 'owl', 'lion', 'parrot', 'panda', 'duck', 'turtle', 'sheep', 'penguin', 'kangaroo', 'rabbit', 'hippo', 'hedgehog', 'bear']
     const timeOfCardAnimation = 250
     const startButtonRef = useRef(null)
@@ -266,17 +266,17 @@ function Memory() {
                                 </div>
                                 {/*body*/}
                                 <div className="relative p-6 flex-auto">
-                                    <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                                    <p className="my-4 text-blueGray-500  max-lg:text-4xl text-2xl leading-relaxed">
                                         Comparisons: {comparisons}
                                     </p>
-                                    <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                                    <p className="my-4 text-blueGray-500 max-lg:text-4xl text-2xl leading-relaxed">
                                         Time: {timeString.current}
                                     </p>
                                 </div>
                                 {/*footer*/}
                                 <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                                     <button
-                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2  max-lg:text-2xl text-xl outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                         type="button"
                                         onClick={() => {
                                             setShowModal(false)
@@ -287,7 +287,7 @@ function Memory() {
                                         Exit
                                     </button>
                                     <button
-                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase  max-lg:text-2xl text-xl px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                         type="button"
                                         onClick={() => {
                                             setShowModal(false)
